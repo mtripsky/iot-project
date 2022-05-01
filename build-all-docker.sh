780999de380eb78aa531f46631c8b472f6ebf033
mtripsky <m.tripsky@gmail.com>
2022-05-01 23:31:51 +0200

    Update general settings for docker-compose

--- build-all-docker.sh
+++ build-all-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 (cd iot-sensors-raspberry; ./build-docker-iot-sensors-raspberry.sh)
 (cd iot-persistence; ./build-docker-iot-persistence.sh)
-
+(cd iot-raspberrypi-pins; ./build-docker-iot-raspberrypi-pins)
